db27bfdd641fa4c314e2f84c17bb6b4fcda7096e
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-11-20 18:00:38 -0300

    :recycle: refactor: altera o header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { header } from "./Header.module.css";
-import igniteLogo from "../../Assets/ignite-logo.svg";
+import feedLogo from "../../Assets/newsfeed.png";
 
 export function Header() {
   return (
     <header className={header}>
-      <img src={igniteLogo} alt="Logo da trilha Ignite" />
+      <img src={feedLogo} alt="Logo Feed" />
+      <h1>The Feed</h1>
     </header>
   );
 }
